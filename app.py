a6443c7f5d7809fc816dd8f5b94895e8d6f3f29a
Salman Charolia <charolia@cs.utexas.edu>
2017-10-25 20:18:23 -0500

    Made it flask approved. No more venv file

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
 	return render_template('index.html')
-
-'''@app.route('/about')
+@app.route('/about')
 def about():
-	return render_template('about.html')	'''
+	return render_template('about.html')
+@app.route('/author')
+def author():
+	return render_template('author.html')
+@app.route('/title')
+def title():
+	return render_template('title.html')
+@app.route('/publisher')
+def publisher():
+	return render_template('publisher.html')			
 
 if __name__ == '__main__':
 	app.run()
