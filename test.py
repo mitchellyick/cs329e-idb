48f406b7843d22d42751091800aa909f82fd07b2
Mitchell Yick <mitchyi@cs.utexas.edu>
2017-11-09 12:44:08 -0600

    Update unit test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from create_db import *
+'''from create_db import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
@@ -7,3 +7,30 @@ from sqlalchemy.orm import sessionmaker
 def test_create(self):
 	create_books()
 	r = session.query()
+'''
+import os
+import unittest
+from models import Base, Titles, engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+class DBTestCases(unittest.TestCase):
+    def test_source_insert_1(self):
+        s = Titles(id='20', title = 'C++')
+        session.add(s)
+        session.commit()
+
+
+        r = session.query(Book).filter_by(id = '20').one()
+        self.assertEqual(str(r.id), '20')
+
+        session.query(Book).filter_by(id = '20').delete()
+        session.commit()
+
+if __name__ == '__main__':
+	unittest.main()
