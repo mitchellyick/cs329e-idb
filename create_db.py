944ea578cee8354c351076e72a72a2d3f376d0d4
Salman Charolia <salmancharolia@gmail.com>
2017-11-08 19:47:35 -0600

    Fixing import errors

--- create_db.py
+++ create_db.py
@@ -5,7 +5,7 @@ import json, logging
 # import the following dependencies from SQLAlchmey
 # and the empty database we created into our environment 
 from sqlalchemy.orm import sessionmaker
-from models import db, Authors, Titles, Publishers
+from models import *
 
 # bind the engine to the base class. This makes the connection
 # between our class definitions and the corresponding tables 
