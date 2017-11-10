32353183f78466ad9c3ba6e4a963cb638c98a8a8
Mitchell Yick <mitchellyick@utexas.edu>
2017-11-09 22:14:15 -0600

    Trying to get Travis to pass-late

--- create_db.py
+++ create_db.py
@@ -172,7 +172,7 @@ def create_aut():
             for thinge in publishersstep1:
                 pub = thinge['name']
 
-            newAut =  Authors(born = dob, name = author, education = education, nationality = nat, description = descAuth, alma_mater = alma, wikipedia_url = wiki, image_url = a_im_url, title = title, publisher = pub,val = counter)
+            newAut =  Authors(born = dob, name = author, education = education, nationality = nat, description = descAuth, alma_mater = alma, wikipedia_url = wiki, image_url = a_im_url, title = title, publisher = pub)
             if session.query(Authors).filter(Authors.name == newAut.name).count() ==0: 
                 session.add(newAut)
                 session.commit()
@@ -211,14 +211,12 @@ def create_pub():
             except:
                 pub_wiki = "N/A"
 
-            newPub= Publishers(title= title, author= author, name = pub, owner = owner, description = descpub, image_url = pub_im_url, web_link = website, wiki_url = pub_wiki, val = 0)
+            newPub= Publishers(title= title, author= author, name = pub, owner = owner, description = descpub, image_url = pub_im_url, web_link = website, wiki_url = pub_wiki)
 
         
             if session.query(Publishers).filter(Publishers.name == newPub.name).count() ==0: 
                 session.add(newPub)
                 session.commit()
-                counter+=1
-
         
 create_tables()
 create_aut()
