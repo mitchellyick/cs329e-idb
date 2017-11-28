e0af6881c9342cd39a7727266a5db94f04e10d92
Salman Charolia <salmancharolia@gmail.com>
2017-11-28 02:26:03 -0600

    Added all single page templates

--- app.py
+++ app.py
@@ -60,6 +60,51 @@ def publisher_page(publisher_name):
 	
 	return render_template('i_publisher.html', info=info, publisher_name=publisher_name, owner=owner, publisher_description = publisher_description, \
 	website=website, publisher_image_url=publisher_image_url, lia=lia, lib=lib)
+@app.route('/title/<book_title>')
+def title_page(book_title):
+	info = session.query(Publishers).all()
+	authors = session.query(Authors).all()
+	books = session.query(Titles).all()
+	for i in books:
+		if i.title ==book_title:
+			author = i.author
+			google_id = i.google_id
+			isbn = i.isbn
+			title_image_url = i.image_url
+			pub_date = i.publication_date
+			book_descrip = i.description
+			pub = i.publisher
+
+	
+	return render_template('i_title.html', info=info, book_title=book_title, author=author, google_id = google_id, \
+	isbn=isbn, title_image_url=title_image_url, pub_date=pub_date, book_descrip=book_descrip, pub = pub)
+
+@app.route('/author/<author_name>')
+def author_page(author_name):
+	info = session.query(Publishers).all()
+	authors = session.query(Authors).all()
+	books = session.query(Titles).all()
+	for i in authors:
+		if i.name ==author_name:
+			dob = i.born 
+			author_description = i.description
+			education = i.education
+			nationality = i.nationality
+			alma_mater = i.alma_mater
+			wiki_url = i.wikipedia_url
+			
+	lip = []
+	for i in info:
+		if i.author == author_name:
+			if i.name not in lip:
+				lip.append(i.name)
+	lib = []
+	for i in books:
+		if i.author == author_name:
+			if i.title not in lib:
+				lib.append(i.title)
+	return render_template('i_authors.html', info=info, author_name=author_name, dob=dob, author_description = author_description, \
+	education=education, nationality=nationality, alma_mater=alma_mater, wiki_url=wiki_url, publisher = lip, title = lib)
 
 @app.route('/unit_tests')
 def unit_tests():
